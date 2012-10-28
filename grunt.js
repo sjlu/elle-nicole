60e37a628a3f6cc3363073f45c14cc30070b5564
Steven Lu <tacticalazn@gmail.com>
2012-10-28 02:08:39 -0400

    Fixing npm modules/grunt

--- grunt.js
+++ grunt.js
@@ -24,25 +24,8 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: '<json:package.json>',
 		meta: {},
-		server: {
-			port: 8000,
-			base: 'public/'
-		},
 		files: {
 			js: [
-				'assets/raw/twitter-bootstrap/js/bootstrap-affix.js',
-				'assets/raw/twitter-bootstrap/js/bootstrap-alert.js',
-				'assets/raw/twitter-bootstrap/js/bootstrap-button.js',
-				'assets/raw/twitter-bootstrap/js/bootstrap-carousel.js',
-				'assets/raw/twitter-bootstrap/js/bootstrap-collapse.js',
-				'assets/raw/twitter-bootstrap/js/bootstrap-dropdown.js',
-				'assets/raw/twitter-bootstrap/js/bootstrap-modal.js',
-				'assets/raw/twitter-bootstrap/js/bootstrap-tooltip.js',
-				'assets/raw/twitter-bootstrap/js/bootstrap-popover.js',
-				'assets/raw/twitter-bootstrap/js/bootstrap-scrollspy.js',
-				'assets/raw/twitter-bootstrap/js/bootstrap-tab.js',
-				'assets/raw/twitter-bootstrap/js/bootstrap-transition.js',
-				'assets/raw/twitter-bootstrap/js/bootstrap-typeahead.js',
 				'assets/js/*.js',
 			],
 			less: [
@@ -55,49 +38,39 @@ module.exports = function(grunt) {
 		concat: {
 			js: {
 				src: '<config:files.js>',
-				dest: 'public/assets/js/app.js'
+				dest: 'assets/js/app.js'
 			}
 		},
 		min: {
 			js: {
 				src: '<config:files.js>',
-				dest: 'public/assets/js/app.js'
+				dest: 'assets/js/app.js'
 			}
 		},
 		recess: {
 			min: {
 				src: '<config:files.less>',
-				dest: 'public/assets/css/style.css',
+				dest: 'assets/css/style.css',
 				options: recessOptions(true)
 			},
 			max: {
 				src: '<config:files.less>',
-				dest: 'public/assets/css/style.css',
+				dest: 'assets/css/style.css',
 				options: recessOptions(false)
-			}
-		},
-		shell: {
-			sync: {
-	            command: 'rm -rf public/; mkdir -p public/assets/font; mkdir -p public/assets/img; cp assets/raw/font-awesome/font/* public/assets/font/; cp assets/raw/twitter-bootstrap/img/* public/assets/img/; cp *.html public/; cp assets/img/* public/assets/img/; cp CNAME public/',
-	            stdout: true
-			},
-			deploy: {
-	            command: 'git stash; git push origin :gh-pages; git branch -D gh-pages; git symbolic-ref HEAD refs/heads/gh-pages; rm -rf .git/index /tmp/public /tmp/node_modules /tmp/raw; mv public /tmp; mv node_modules /tmp; mv assets/raw /tmp; git clean -fdx; mv /tmp/public/* .; git add .; git commit -m "auto-generated deployment to gh-pages"; git checkout master; mv /tmp/node_modules .; cp -R /tmp/raw assets/; git stash apply',
-	            stdout: true
 			}
 		},
 		watch: {
 			less: {
 				files: ['assets/less/*.less', 'assets/less/**/*.less'],
-				tasks: 'shell:sync concat recess:max'
+				tasks: 'concat recess:max'
 			},
 			js: {
 				files: '<config:files.js>',
-				tasks: 'shell:sync concat recess:max'
+				tasks: 'concat recess:max'
 			},
 			html: {
 				files: '<config:files.html>',
-				tasks: 'shell:sync concat recess:max'
+				tasks: 'concat recess:max'
 			}
 		},
 		jshint: {
@@ -119,6 +92,6 @@ module.exports = function(grunt) {
 	});
 
 	// Default task.
-	grunt.registerTask('default', 'shell:sync concat recess:max server watch');
-  	grunt.registerTask('deploy', 'shell:sync min recess:min shell:deploy');
+	grunt.registerTask('default', 'concat recess:max');
+  	grunt.registerTask('deploy', 'min recess:min');
 };

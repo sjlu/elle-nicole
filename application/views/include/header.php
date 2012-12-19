79e50f0524008825002eebab95a0af2e5a705d16
Steven Lu <tacticalazn@gmail.com>
2012-12-19 14:45:37 -0500

    No retina for now.

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -12,7 +12,7 @@
    <link href="<?= base_url('assets/css/style.css') ?>" rel="stylesheet">
 
    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-   <script src="<?= base_url('assets/import/retina.js') ?>"></script>
+   <!-- <script src="<?= base_url('assets/import/retina.js') ?>"></script> -->
 	<!-- <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script> -->
    <!-- <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/0.9.0/lodash.min.js"></script> -->
 	<script src="<?= base_url('assets/js/app.js') ?>"></script>
